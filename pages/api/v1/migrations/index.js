a0077980c29565c9a4c5260cdd3492febc77971a
Bruno Cayres Messias <brunocmessias@proton.me>
2024-03-29 22:55:30 +0000

    Adding Migrations applications and configure jest to read environments

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,33 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+
+export default async function migrations(request, response) {
+  const defaultMigrationsOptions = {
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+    response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
+      dryRun: false,
+    });
+
+    if (migratedMigrations.length > 0) {
+      response.status(201).json(migratedMigrations);
+    }
+
+    response.status(200).json(migratedMigrations);
+  }
+
+  response.status(405).end();
+}
