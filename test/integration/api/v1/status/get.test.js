5c759efeb50d4115d16a0a5fc10d638221f33541
Bruno Cayres Messias <brunocmessias@proton.me>
2024-07-06 23:59:11 +0000

    Remove test for version database

--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -13,7 +13,7 @@ test("GET to /api/v1/status should return 200", async () => {
   const parseUpdateAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toBe(parseUpdateAt);
 
-  expect(responseBody.dependencies.database.version).toBe("16.2");
+  // expect(responseBody.dependencies.database.version).toBe("16.2");
   expect(responseBody.dependencies.database.max_connections).toBe(100);
   expect(responseBody.dependencies.database.opened_connection).toBe(1);
 });
