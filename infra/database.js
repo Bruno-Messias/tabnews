31e1b1784b56e000730edf2f0fb8de2c5beefad2
Bruno Cayres Messias <brunocmessias@proton.me>
2025-02-02 21:34:29 +0000

    refactor: add new custom erro `ServiceError` to database implementation

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors";
 
 async function query(queryObject) {
   let client;
@@ -8,8 +9,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.error(error);
-    throw error;
+    const ServiceErrorObject = new ServiceError({
+      message: "Erro na conexão com o banco ou na Query.",
+      cause: error,
+    });
+    throw ServiceErrorObject;
   } finally {
     await client?.end();
   }
