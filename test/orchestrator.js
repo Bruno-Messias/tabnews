23256a42f2ad8987a0d1037550bfc01d86e72d79
Bruno Cayres Messias <brunocmessias@hotmail.com>
2025-05-16 14:58:55 +0000

    fix: mistyping in import

--- test/orchestrator.js
+++ test/orchestrator.js
@@ -1,5 +1,5 @@
 import retry from "async-retry";
-import { faker, Faker } from "@faker-js/faker";
+import { faker } from "@faker-js/faker";
 
 import database from "infra/database";
 import migrator from "models/migrator";
